25ea419254128920c7e448754ba1a111ace2eea0
light <286224275@qq.com>
2024-01-11 19:02:13 +0800

    2023.11.20

--- lw_Method10.py
+++ lw_Method10.py
@@ -2865,9 +2865,12 @@ class Caozuolei(Caozuolei1):
                     t31 = Thread(target=self.FuBen_INFO66)  # 定义线程t2，
                     t31.start()  # 开始运行t1线程
                     dt.press('down')
+                    time.sleep(0.1)
+                    dt.press('down')
+                    time.sleep(0.1)
                     dt.press('down')
                     self.FuBen_INFO6(0, 6)
-                    dt.press('down')
+
                     time.sleep(0.7)
                     dt.press('9')
                     time.sleep(0.75)
@@ -5550,7 +5553,7 @@ if __name__ == '__main__':
     # x = [[150, 290, 1], [270, 280, 2], [380, 215, 3], [490, 215], 4, [719, 285, 5], [80, 501, 6]]
     x = [[134, 231, 1], [274, 258, 2], [412, 244, 3], [556, 247, 4], [691, 256, 5], [67, 464, 6], [204, 466, 7],
          [350, 487, 8], [450, 487, 9], [550, 487, 10]]
-    for aa in range(6, 10):  # 打图设置ddddg
+    for aa in range(0, 10):  # 打图设置ddddg
         # c.Set_Dict(1, '1测试2.txt')
         # c.Set_Dict(0, 'test3.t1xt')f
         if aa == 11 and pvp == 1:  # aa
@@ -5750,7 +5753,7 @@ if __name__ == '__main__':
                 # c.nvQiGong(n, 0.62, 0.26, 0)  # 2p 睡眠不足丶
                 #
             elif pvp == 2:
-                c.nvQiGong(n, 0.56, 0.3, 0, 0, *canshu)  # 2p 睡眠不足丶
+                c.nvQiGong(n, 0.59, 0.3, 0, 0, *canshu)  # 2p 睡眠不足丶
             elif pvp == 3:
                  c.nvQiGong(n, 0.59, 0.26, 0, 0, *canshu)  # 3p 气功运气 2023 11 16
             elif pvp == 4:
